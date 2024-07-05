64b893eb4dd686156ebe1105f4aa23d9a7a17a1c
Erik <erikpersson0884@gmail.com>
2024-07-05 23:13:23 +0200

    added navigation to header and a page for cv

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -10,7 +10,7 @@ import info from '../../info.json';
 const Header: React.FC = () => {
     useEffect(() => { // hide header on first page
         const handleScroll = () => {
-            const header = document.getElementById('sticky-header') as HTMLElement;
+            const header = document.getElementById('pageHeader') as HTMLElement;
             const headerHeight = header.offsetHeight;
 
             // const heightToShowHeader = window.innerHeight;
@@ -29,15 +29,25 @@ const Header: React.FC = () => {
         };
     }, []);
 
+
+
     return (
-        <header id="sticky-header">
+        <header id="pageHeader">
             <Link to="/">
                 <img className="websiteLogo" src="images/erikpersson-logo.png" alt="Erik Persson Logo" />
             </Link>
 
+            <nav className="pageNavigation">
+                <a href="#projects">Projects</a>
+
+                <a href="files/cv.pdf">CV</a>
+
+                <Link to="/contact">Contact me</Link>
+            </nav>
+
             <nav>
-                <ul className='noUlFormatting'>
-                    {info.socials.map((social, index) => (
+                <ul className='noUlFormatting hidden'>
+                    {info.socials.map((social, index) => (  
                         <li key={index}>
                             <a href={social.url} target='_blank' rel='noreferrer'>
                                 <img src={social.icon} alt={social.name} />
---
+++ frontend/src/Pages/list-app/list-app.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./list-app.css";
+
+
+
+const ListApp = () => {
+    return (
+        <div>
+            <h1>List App</h1>
+
+            <img src="images/projects/list-app-1.png" alt="List App" className="project-image" />
+        </div>
+    );
+};
+
+export default ListApp;
