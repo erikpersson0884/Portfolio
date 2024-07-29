a1abc1a8f5d56853543181a5cc10f4f7e2b253fd
Erik <erikpersson0884@gmail.com>
2024-07-29 18:02:19 +0200

    made footer logos glow when hovered

--- frontend/src/Components/Footer/Footer.tsx
+++ frontend/src/Components/Footer/Footer.tsx
@@ -4,16 +4,20 @@ import './Footer.css';
 import info from '../../info.json';
 
 const Footer: React.FC = () => {
+    const social = info.socials[0]
+
     return (
         <footer className='pageFooter'>
-            <a href={"mailto:"+info.contact.email} className="footerEmail">{info.contact.email}</a>
+            <a className="footerEmail" href={"mailto:"+info.contact.email}>{info.contact.email}</a>
 
             <nav className="footerLogos">
                 {info.socials.map((social, index) => (
-                        <a href={social.url} key={index}>
-                            <img src={social.icon} alt={social.name} />
+                        <a href={social.url} key={1433}>
+                            <svg>
+                                <path d={social.path}></path>
+                            </svg>
                         </a>
-                ))}
+                ))} 
             </nav>
         </footer>
     );
